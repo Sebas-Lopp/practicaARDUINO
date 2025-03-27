8f48bb69b47501350fcec641a3e03bb7ef0c6681
sebas <sebas@LAPTOP-9BCMTOQP>
2025-03-26 22:44:52 -0600

    Agregar archivos de proyecto.

---
+++ practicaARDUINO/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace practicaARDUINO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncender = new System.Windows.Forms.Button();
+            this.btnApagado = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEncender
+            // 
+            this.btnEncender.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnEncender.Font = new System.Drawing.Font("Cooper Black", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEncender.Location = new System.Drawing.Point(304, 61);
+            this.btnEncender.Name = "btnEncender";
+            this.btnEncender.Size = new System.Drawing.Size(129, 80);
+            this.btnEncender.TabIndex = 0;
+            this.btnEncender.Text = "ENCENDER";
+            this.btnEncender.UseVisualStyleBackColor = false;
+            this.btnEncender.Click += new System.EventHandler(this.btnEncender_Click);
+            // 
+            // btnApagado
+            // 
+            this.btnApagado.BackColor = System.Drawing.Color.Aquamarine;
+            this.btnApagado.Font = new System.Drawing.Font("Cooper Black", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApagado.Location = new System.Drawing.Point(302, 250);
+            this.btnApagado.Name = "btnApagado";
+            this.btnApagado.Size = new System.Drawing.Size(131, 82);
+            this.btnApagado.TabIndex = 1;
+            this.btnApagado.Text = "APAGADO";
+            this.btnApagado.UseVisualStyleBackColor = false;
+            this.btnApagado.Click += new System.EventHandler(this.btnApagado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnApagado);
+            this.Controls.Add(this.btnEncender);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncender;
+        private System.Windows.Forms.Button btnApagado;
+    }
+}
+
---
+++ practicaARDUINO/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace practicaARDUINO
+{
+    public partial class Form1 : Form
+    {
+        public SerialPort ArduinoPort { get; }
+        public Form1()
+        {
+            InitializeComponent();
+            ArduinoPort = new System.IO.Ports.SerialPort();
+            ArduinoPort.PortName = "COM3";
+            ArduinoPort.BaudRate = 9600;
+            ArduinoPort.Open();
+
+            this.FormClosing += Form1_FormClosing;
+            this.btnApagado.Click += btnApagado_Click;
+            this.btnEncender.Click += btnEncender_Click;
+        }
+
+        private void btnEncender_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("a");
+        }
+        private void btnApagado_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("b");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (ArduinoPort.IsOpen) ArduinoPort.Close();
+        }
+    }
+}
